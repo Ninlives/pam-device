3ee080861b25a46cd494e5017b395ec460786424
mlatus <wqseleven@gmail.com>
2020-03-17 11:19:45 +0800

    adhoc modify, move config to /var for better privacy

--- src/pamdevice/__init__.py
+++ src/pamdevice/__init__.py
@@ -24,10 +24,25 @@
 # SOFTWARE.
 
 import os
-from os.path import expanduser
+import getpass
 
+try:
+	user = os.getlogin()
+except Exception:
+	user = os.environ.get("SUDO_USER")
 
-CONFIG_DIR = os.path.join(expanduser('~'), '.config', 'pam-device')
+# If that fails, try to get the direct user
+if user == "root" or user is None:
+	env_user = getpass.getuser().strip()
+
+	# If even that fails, error out
+	if env_user == "":
+		print("Could not determine user, please use the --user flag")
+		sys.exit(1)
+	else:
+		user = env_user
+
+CONFIG_DIR = os.path.join('var', 'lib', 'pam-device', user)
 CONFIG_FILE = os.path.join(CONFIG_DIR, 'pam-device.json')
 PARAMS = {'usb': [],
           'bluetooth-scan-timeout': 8,
--- src/pamdevicegui/__init__.py
+++ src/pamdevicegui/__init__.py
@@ -1,8 +1,23 @@
 import os
-from os.path import expanduser
 
+try:
+	user = os.getlogin()
+except Exception:
+	user = os.environ.get("SUDO_USER")
 
-CONFIG_DIR = os.path.join(expanduser('~'), '.config', 'pam-device')
+# If that fails, try to get the direct user
+if user == "root" or user is None:
+	env_user = getpass.getuser().strip()
+
+	# If even that fails, error out
+	if env_user == "":
+		print("Could not determine user, please use the --user flag")
+		sys.exit(1)
+	else:
+		user = env_user
+
+CONFIG_DIR = os.path.join('var', 'lib', 'pam-device', user)
+
 CONFIG_FILE = os.path.join(CONFIG_DIR, 'pam-device.json')
 PARAMS = {'usb': [],
           'bluetooth-scan-timeout': 8,
